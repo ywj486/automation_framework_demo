b17b692987ca7027d4104ec22a3f171c63e430cd
ywj <ywj_486@163.com>
2018-08-17 16:07:55 +0800

    初使用自动化框架

--- testsuits/baidusearch.py
+++ testsuits/baidusearch.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 from framework.browser_engine import BrowserEngine
-
+from pageobjects.baidu_homepage import HomePage
 
 class BaiduSearch(unittest.TestCase):
     def setUp(self):
@@ -24,6 +24,24 @@ class BaiduSearch(unittest.TestCase):
         这里一定要test开头，把测试逻辑代码封装到一个test开头的方法里。
         :return:
         """
+        homepage = HomePage(self.driver)
+        homepage.type_search('selenium')  # 调用页面对象中的方法
+        homepage.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
+        time.sleep(2)
+        homepage.get_windows_img()  # 调用基类截图方法
+        try:
+            assert 'selenium' in homepage.get_page_title()  # 调用页面对象继承基类中的获取页面标题方法
+            print('Test Pass.')
+        except Exception as e:
+            print('Test Fail.', format(e))
+
+    '''
+    
+    def test_baidu_search(self):
+        """
+        这里一定要test开头，把测试逻辑代码封装到一个test开头的方法里。
+        :return:
+        """
         self.driver.find_element_by_id('kw').send_keys('selenium')
         time.sleep(1)
         self.driver.find_element_by_id("su").click()
@@ -33,7 +51,8 @@ class BaiduSearch(unittest.TestCase):
             print('Test Pass.')
         except Exception as e:
             print('Test Fail.', format(e))
-
+            
+    '''
 
 if __name__ == '__main__':
     unittest.main()
